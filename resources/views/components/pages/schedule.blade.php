02aa951c0ab3b7b06ca1830ce62e66e09d2c25f0
Ananda Rizky Kurniawan <qq.kurniadi445@gmail.com>
2024-05-30 21:16:39 +0700

    Tambah formulir untuk menambah jadwal

--- resources/views/components/pages/schedule.blade.php
+++ resources/views/components/pages/schedule.blade.php
@@ -17,6 +17,14 @@
 
 @section('content')
 <link rel="stylesheet" href="{{ asset('assets/css/schedule.css')}}" />
+    <div class="d-flex justify-content-end mb-3">
+        <button
+            class="btn btn-success"
+            data-bs-target="#modal-tambah-jadwal"
+            data-bs-toggle="modal"
+            type="button"
+        >Add Schedule</button>
+    </div>
 <table id="example1" class="table table-bordered table-striped">
           <thead>
           <tr>
@@ -52,6 +60,48 @@
         </tbody>
           @endif
         </table>
+{{-- modal --}}
+<div class="fade modal" id="modal-tambah-jadwal">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="fs-5 modal-title">Tambah Jadwal</h5>
+                <button class="btn-close" data-bs-dismiss="modal" type="button"></button>
+            </div>
+            <div class="modal-body">
+                <form method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="pemilih-pengguna">Pengguna</label>
+                        <select class="form-control" id="pemilih-pengguna">
+                            <option selected>Pilih pengguna</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="pemilih-properti">Properti</label>
+                        <select class="form-control" id="pemilih-properti">
+                            <option selected>Pilih properti</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="masukan-tanggal">Tanggal</label>
+                        <input class="form-control" id="masukan-tanggal" type="date">
+                    </div>
+                    <div class="mb-3">
+                        <label for="masukan-pukul">Pukul</label>
+                        <input class="form-control" id="masukan-pukul" type="time">
+                    </div>
+                    <div class="mb-3">
+                        <label for="masukan-catatan">Catatan</label>
+                        <input class="form-control" id="masukan-catatan" name="catatan" type="text">
+                    </div>
+                    <button class="btn btn-primary" type="submit">Simpan</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- modal --}}
 
         <div class="modal fade" id="detailModal" tabindex="-1" aria-labelledby="detailModalLabel" aria-hidden="true">
           <div class="modal-dialog">
