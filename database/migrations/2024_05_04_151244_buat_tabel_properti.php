dad94966db7a335a7f08f0c1e767406922c9320b
Ananda Rizky Kurniawan <qq.kurniadi445@gmail.com>
2024-05-31 00:46:52 +0700

    Tambah migration untuk properti

---
+++ database/migrations/2024_05_04_151244_buat_tabel_properti.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properti', function (Blueprint $table) {
+            $table->increments('id_properti');
+            $table->string('nama_properti', 50);
+            $table->unsignedBigInteger('harga');
+            $table->string('alamat', 100);
+            $table->text('deskripsi');
+            $table->unsignedSmallInteger('luas');
+            $table->unsignedTinyInteger('jumlah_garasi');
+            $table->unsignedTinyInteger('jumlah_lantai');
+            $table->unsignedTinyInteger('jumlah_kamar_mandi');
+            $table->unsignedTinyInteger('jumlah_kamar_tidur');
+            $table->enum('status', ['Siap', 'Tertunda', 'Terjual']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
--- database/migrations/2024_05_10_103317_create_images_table.php
+++ database/migrations/2024_05_10_103317_create_images_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\Property;
+use App\Models\Properti;
 return new class extends Migration
 {
     /**
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Property::class);
-            $table->string('image'); 
+            $table->foreignIdFor(Properti::class);
+            $table->string('image');
             $table->timestamps();
         });
     }
