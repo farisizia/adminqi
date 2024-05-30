75a2132d7e98de6ffaa7039e4af21ff5568b5066
Ananda Rizky Kurniawan <qq.kurniadi445@gmail.com>
2024-05-30 20:30:37 +0700

    Tambah migration untuk jadwal

---
+++ database/migrations/2024_05_30_131917_buat_tabel_jadwal.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jadwal', function (Blueprint $table) {
+            $table->increments('id_jadwal');
+            $table->unsignedBigInteger('id_pengguna');
+            $table->unsignedBigInteger('id_properti');
+            $table->date('tanggal');
+            $table->time('pukul');
+            $table->tinyText('catatan');
+            $table->boolean('jadwal_diterima')->default(false);
+
+            $table->foreign('id_pengguna')->references('id')->on('data_user');
+            $table->foreign('id_properti')->references('id')->on('property');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
