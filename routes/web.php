4de7fed0686e06d1cfc6ddea6118bb1a6b24fada
Ananda Rizky Kurniawan <qq.kurniadi445@gmail.com>
2024-05-30 23:49:15 +0700

    Tambah rute untuk halaman masuk

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AutentikasiController;
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Login_Controller;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\Data_UserController;
 use App\Http\Controllers\DashboardController;
@@ -18,7 +17,7 @@ use App\Http\Controllers\DashboardController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/homebjhgdcts',[DashboardController::class, 'index']) ->name('components.pages.home'); 
+Route::get('/homebjhgdcts',[DashboardController::class, 'index']) ->name('components.pages.home');
 
 Route::get('/master', function () {
     return view('components.template.master');
@@ -49,9 +48,11 @@ Route::get('/admin', function () {
 // });
 
 
-// === New ===
-Route::get('/', [Login_Controller::class, 'index'])->name('admin.login');
-Route::post('/', [Login_Controller::class, 'authenticate'])->name('admin.login.auth');
+Route::prefix('autentikasi')->group(function () {
+    Route::name('autentikasi.')->group(function () {
+        Route::get('/masuk', [AutentikasiController::class, 'masuk'])->name('masuk');
+    });
+});
 
 Route::group(['middleware' => ['admin.auth']], function () {
     Route::view('/home', 'components.pages.home')->name('admin.dashboard');
